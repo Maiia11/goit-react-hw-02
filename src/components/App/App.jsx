ba3f3d01aa80ea7935603d1a47195f8913136eb3
Maiia11 <149806655+Maiia11@users.noreply.github.com>
2024-05-21 13:03:13 +0300

    add Options components

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,16 +3,26 @@ import { useState } from 'react'
 import Descriptions from '../Descriptions/Descriptions'
 import './App.css'
 import Feedback from '../Feedback/Feedback'
+import Options from '../Options/Options'
+
+
+
 
 function App() {
 
-  
+  const [state, setState] = useState({
+  good: 0,
+	neutral: 0,
+	bad: 0
+  })
 
   return (
     <>
-    <Descriptions />
+      <Descriptions />
+      
+      <Options/>
 
-    <Feedback/>
+    <Feedback state={state}/>
 
      </> 
 
--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,13 +1,5 @@
-import { useState } from "react"
 
-
-const Feedback = () => {
-    const [state, setState] = useState({
-  good: 0,
-	neutral: 0,
-	bad: 0
-  })
-    // const {good, neutral, bad } = state;
+const Feedback = ({state}) =>{
     
     return (
         <div>
